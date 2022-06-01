3f2eb994cfdec343980fedb2952e9d65ec1c5eba
Ilya Nesterov <inesterov@pushwoosh.com>
2022-06-01 20:18:02 +0200

    Fix search margin

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ import { getTokens } from './pages/Auth/helpers/tokens';
 import { GlobalStyle } from './styles';
 import { Router } from './routes';
 
-const httpLink = new HttpLink({ uri: 'http://185.137.234.69:3000/graphql' });
-// const httpLink = new HttpLink({ uri: 'https://api.autothoughts.xyz/' });
+// const httpLink = new HttpLink({ uri: 'http://185.137.234.69:3000/graphql' });
+const httpLink = new HttpLink({ uri: 'https://api.autothoughts.xyz/' });
 
 const authMiddleware = new ApolloLink((operation, forward) => {
   const tokens = getTokens();
